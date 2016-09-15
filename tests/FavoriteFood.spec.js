00106da45800acb0601c568dbb7a5691283aa4d9
hudbuz <hzb2@cornell.edu>
2016-09-15 19:18:38 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -5,6 +5,12 @@ describe('Favorite Food Filter', function () {
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
+    FavoriteFood  = $filter('favoriteFood')
+    var foods = {1 : {'favoriteFood': 'apple'}, 2: {'favoriteFood': 'pear'}, 3: {'favoriteFood': 'apple'}} 
+
+    it('should return a users favorite food', function() {
+      expect(FavoriteFood(foods, 'apple')).length.toEqual(2)
+    })
 	}));
 
 	
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -5,6 +5,13 @@ describe('removeAllVowels Filter', function () {
 
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
+    remove = $filter('removeAllVowels')
+    var hud = 'hudson'
+
+    it('should remove all vowels', function () {
+
+      expect(remove(hud)).toEqual('hdsn')
+    })
 	}));
 
 });
